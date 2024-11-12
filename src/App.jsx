7dc9b337abba43411f6d01d20f47a4c0fe7c7e13
이아름 <areum6321@gmail.com>
2024-11-12 15:08:53 +0900

    design: Image 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 function App() {
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
