9abfb960cf2ff46923990ca3e389a46bede54a2d
이아름 <areum6321@gmail.com>
2024-11-12 19:54:17 +0900

    feat: 기존 일기 Update 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function reduecer(state, action) {
   switch (action.type) {
     case "CREATE":
       return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
   }
 }
 
@@ -46,6 +50,18 @@ function App() {
     });
   };
 
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
   return (
     <>
       <button
@@ -54,6 +70,13 @@ function App() {
         }}
       >
         일기 추가 테스트
+      </button>
+      <button
+        onClick={() => {
+          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+        }}
+      >
+        일기 수정 테스트
       </button>
       <Routes>
         <Route path="/" element={<Home />} />
