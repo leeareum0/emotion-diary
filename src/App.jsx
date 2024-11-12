ff11499a59e7a015e90e0e22a8f5af0c3c314280
이아름 <areum6321@gmail.com>
2024-11-12 18:33:57 +0900

    feat: 일기 관리 기능 구현, 임시 데이터 추가 및 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useReducer } from "react";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
-import Buttom from "./components/Button";
-import Header from "./components/Header";
 import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound";
+
+// 임시 일기 데이터
+const mockData = [
+  {
+    id: 1,
+    createData: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createData: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reduecer(state, action) {
+  return state;
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reduecer, mockData);
+
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Buttom text={"Left"} />}
-        rightChild={<Buttom text={"Right"} />}
-      />
-
-      <Buttom
-        text={"123"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Buttom
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Buttom
-        text={"123"}
-        type={"NAGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
