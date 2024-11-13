e89ed38d477938733b944912a29e03c1af6947f6
이아름 <areum6321@gmail.com>
2024-11-13 19:48:20 +0900

    design: Home Page UI 구현, Home 컴포넌트에 필요한 컴포넌트 import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import Buttom from "../components/Button";
+import DiaryList from "../components/DiaryList";
+import Header from "../components/Header";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={"2024년 11월"}
+        leftChild={<Buttom text={"<"} />}
+        rightChild={<Buttom text={">"} />}
+      />
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
