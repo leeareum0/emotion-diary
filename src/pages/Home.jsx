2fe5200f0e0de1e513622d34f49272825ba151be
이아름 <areum6321@gmail.com>
2024-11-14 17:15:33 +0900

    feat: Header 부분에 날짜 변경 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
+import { useState, useContext } from "react";
 import Buttom from "../components/Button";
 import DiaryList from "../components/DiaryList";
 import Header from "../components/Header";
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 11월"}
-        leftChild={<Buttom text={"<"} />}
-        rightChild={<Buttom text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Buttom onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Buttom onClick={onIncreaseMonth} text={">"} />}
       />
       <DiaryList />
     </div>
