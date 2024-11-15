70ad97ed04ec732fc6e3f2c15f962d9d6c5ecfea
이아름 <areum6321@gmail.com>
2024-11-15 16:56:39 +0900

    feat: Edit Page 뒤로 가기 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,16 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
 
   return (
     <div>
       <Header
         title={"일기 수정하기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
         rightChild={<Button text={"삭제하기"} type={"NAGATIVE"} />}
       />
       <Editor />
