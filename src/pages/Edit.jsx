d647db709105330c94c13c24e9b1e0fbb4360593
이아름 <areum6321@gmail.com>
2024-11-15 16:42:45 +0900

    design: Edit Page UI 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기 수정 페이지입니다.</div>;
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+        rightChild={<Button text={"삭제하기"} type={"NAGATIVE"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
