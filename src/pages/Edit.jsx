84178fd7eaf285ae69022960cf7e6e058970884c
이아름 <areum6321@gmail.com>
2024-11-15 17:10:17 +0900

    feat: Edit Page 일기 삭제 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -2,17 +2,30 @@ import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않습니다.")) {
+      // 일기 삭제 로직
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
 
   return (
     <div>
       <Header
         title={"일기 수정하기"}
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
-        rightChild={<Button text={"삭제하기"} type={"NAGATIVE"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NAGATIVE"} />
+        }
       />
       <Editor />
     </div>
