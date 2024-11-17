8d137350708b2281917677714ef14f49b9e65b45
이아름 <areum6321@gmail.com>
2024-11-18 01:16:00 +0900

    feat: Diary Page 감정 및 일기 내용 불러오기 기능 구현

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,11 +1,20 @@
-import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Viewer from "../components/Viewer";
+import useDiary from "../hooks/useDiary";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Diary = () => {
   const params = useParams();
   const nav = useNavigate();
+
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩 중..</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
 
   return (
     <div>
@@ -16,7 +25,7 @@ const Diary = () => {
           <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
         }
       />
-      <Viewer />
+      <Viewer emotionId={emotionId} content={content} />
     </div>
   );
 };
