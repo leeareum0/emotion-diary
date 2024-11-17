2ddb7836971ddd17b3f76490afbd0e7886987eff
이아름 <areum6321@gmail.com>
2024-11-18 01:31:15 +0900

    feat: Diary Page 해당 날짜 불러오기 기능 구현

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
 import { useParams, useNavigate } from "react-router-dom";
+import { getStringDate } from "../util/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
@@ -15,11 +16,12 @@ const Diary = () => {
   }
 
   const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringDate(new Date(createdDate));
 
   return (
     <div>
       <Header
-        title={"yyyy-mm-dd 기록"}
+        title={`${title} 기록`}
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
         rightChild={
           <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
